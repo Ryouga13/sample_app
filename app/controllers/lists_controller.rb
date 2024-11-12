c374bbbcd605e00d1dbcdab84f8259d71a480b0e
Ryouga13 <184459763>
2024-11-12 08:02:07 +0000

    Add 3章【一覧画面を表示しよう】

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,15 +7,20 @@ class ListsController < ApplicationController
   end
 
   def create
+
     # １.&2. データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
     # 4. トップ画面へリダイレクト
     redirect_to '/top'
+
   end
 
   def index
+
+    @lists = List.all  
+    
   end
 
   def show
